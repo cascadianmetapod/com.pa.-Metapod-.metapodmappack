c3094207f5b9fd52ca6640a1e20f5cb33776f408
cascadianmetapod <dudeman@live.ca>
2016-12-22 07:28:24 -0800

    removed biome support

--- ui/mods/metapod/maplist.js
+++ ui/mods/metapod/maplist.js
@@ -1,17 +1,13 @@
 ﻿cShareSystems.load_pas("(Metapod)", [
 	"coui://ui/mods/metapod/systems/rocky.pas",
 	"coui://ui/mods/metapod/systems/cornucopia.pas",
-	"coui://ui/mods/metapod/systems/bitzin.pas",
-	"coui://ui/mods/metapod/systems/burn_the_rainforest.pas",
-	"coui://ui/mods/metapod/systems/marblegumdrop.pas",
-	"coui://ui/mods/metapod/systems/space_litter.pas",
-	"coui://ui/mods/metapod/systems/gamma_system.pas",
+	
 	"coui://ui/mods/metapod/systems/bottleneck.pas",
 	"coui://ui/mods/metapod/systems/the_progenitor.pas",
 	"coui://ui/mods/metapod/systems/bread_crumbs.pas",
 	"coui://ui/mods/metapod/systems/lush.pas",
 	"coui://ui/mods/metapod/systems/sprouts.pas",
-	"coui://ui/mods/metapod/systems/the_great_dessert.pas",
+
 	"coui://ui/mods/metapod/systems/pagan_wars.pas",
 	"coui://ui/mods/metapod/systems/swamplandia.pas",
 	"coui://ui/mods/metapod/systems/ring_world.pas",
