8f5c6437a99f737c715e146a6a5ad15adca43c36
cascadianmetapod <dudeman@live.ca>
2017-08-06 14:50:49 -0700

    Update maplist.js

--- ui/mods/metapod/maplist.js
+++ ui/mods/metapod/maplist.js
@@ -6,13 +6,13 @@
 	"coui://ui/mods/metapod/systems/bottleneck.pas",
 	"coui://ui/mods/metapod/systems/the_progenitor.pas",
 	"coui://ui/mods/metapod/systems/bread_crumbs.pas",
-	"coui://ui/mods/metapod/systems/lush.pas",
+	
 	"coui://ui/mods/metapod/systems/sprouts.pas",
 
 	"coui://ui/mods/metapod/systems/pagan_wars.pas",
 	"coui://ui/mods/metapod/systems/swamplandia.pas",
 	"coui://ui/mods/metapod/systems/ring_world.pas",
-	"coui://ui/mods/metapod/systems/veggie_system.pas",
+	
 	"coui://ui/mods/metapod/systems/river_bank.pas",
 	"coui://ui/mods/metapod/systems/the_progenitor.pas",
 	"coui://ui/mods/metapod/systems/ice_e.pas"
